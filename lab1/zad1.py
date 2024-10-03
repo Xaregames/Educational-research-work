f8e53fc4c04d6ae8009d4456c0290c092071fe6d
Xaregames <72562827+Xaregames@users.noreply.github.com>
2024-10-03 14:35:04 +0500

    Create zad1.py

---
+++ lab1/zad1.py
@@ -0,0 +1,10 @@
+from math import *
+
+x = int(input("Введите X: "))
+y = int(input("Введите Y: "))
+z = int(input("Введите Z: "))
+
+
+print("a = ",round(y+x/(pow(y,2)+abs(pow(x,2)/(y+pow(x,2)))),3))
+print("b = ", round(pow(1+pow(tan(z/2),2),2),3))
+

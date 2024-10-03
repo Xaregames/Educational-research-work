691800a3a21dfe44709afe8118109b9352cc0581
Xaregames <72562827+Xaregames@users.noreply.github.com>
2024-10-03 14:39:32 +0500

    Update zad2.py

--- lab1/zad2.py
+++ lab1/zad2.py
@@ -1,7 +1,9 @@
 a = -3
 h = 0.25
 
-x = float(input("Введите число от 3.25 до 5.75 :"))
 
 
-print(round(pow(x,2)/(pow((x+a),1/2))+0.5*x,3))
+for i in range(325,600,25):
+    x = i/100
+    print(x)
+    print(round(pow(x,2)/(pow((x+a),1/2))+0.5*x,3))

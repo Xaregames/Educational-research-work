c17ceb6627c366a805e5a2bb1ec330ec63609dc2
Xaregames <72562827+Xaregames@users.noreply.github.com>
2024-10-03 14:42:47 +0500

    Update zad3.py

--- lab1/zad3.py
+++ lab1/zad3.py
@@ -4,5 +4,4 @@ from math import *
 for i in range(10,31):
     x = i/10
     print(x)
-    
-    print(round(pow(2,-x)-sqrt(cos(x)+sin(2*x)),2))
+    print(round(pow(2,-x)-sqrt(abs(cos(x)+sin(2*x))),2))
